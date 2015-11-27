8bc90a359295913e63e9ee27e6fd84ac32eb7dd5
Lorenz Brun <lorenz@dolansoft.org>
2015-11-27 12:00:20 +0100

    Fix relative paths

--- dockpipe.sh
+++ dockpipe.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
 DF=.dockpipe/Dockerfile
-TARGET=$2
 RANDOM=$(cat /dev/urandom | tr -cd 'a-f0-9' | head -c 32)
 MODE="root"
-rm -Rf $TARGET/.dockpipe
-mkdir -p $TARGET/.dockpipe
+cd $2
+rm -Rf .dockpipe
+mkdir -p .dockpipe
 while read COMMAND
 do
 	echo $COMMAND
@@ -13,22 +13,22 @@ do
 		SUB)
 			echo "Entering SUB mode"
 			MODE="sub"
-			echo "$COMMAND" | awk '{print "FROM " $2 "\n"}' > $TARGET/$DF-$MODE
+			echo "$COMMAND" | awk '{print "FROM " $2 "\n"}' > $DF-$MODE
 		;;
 		RETURN)
 			RETURN_LID=$(cat /dev/urandom | tr -cd 'a-f0-9' | head -c 32)
-			docker build -t dockpipe-$RETURN_LID -f $DF-$MODE $2
-			CID=$(docker create dockpipe-$RETURN_LID)
-			docker cp "$CID:$(echo "$COMMAND" | awk '{print $2}')" $TARGET/.dockpipe/ret-$RETURN_LID
+			docker build -t dockpipe-$RETURN_LID -f $DF-$MODE .
+			CID=$(docker create dockpipe-$RETURN_LID "")
+			docker cp "$CID:$(echo "$COMMAND" | awk '{print $2}')" .dockpipe/ret-$RETURN_LID
 			docker rm -f $CID
 			echo "Leaving SUB mode"
 			MODE="root"
-			echo "ADD $TARGET/.dockpipe/ret-$RETURN_LID $(echo "$COMMAND" | awk '{print $3}')" >> $TARGET/$DF-$MODE
+			echo "ADD .dockpipe/ret-$RETURN_LID $(echo "$COMMAND" | awk '{print $3}')" >> $DF-$MODE
 		;;
 		*)
-			echo "$COMMAND" >> $TARGET/$DF-$MODE
+			echo "$COMMAND" >> $DF-$MODE
 		;;
 	esac
-done < $2/Dockerfile
-docker build -t $1 -f $DF-root $2
-rm -Rf $TARGET/.dockpipe
+done < Dockerfile
+docker build -t $1 -f $DF-root .
+rm -Rf .dockpipe
